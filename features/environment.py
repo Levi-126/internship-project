06d02dc2a744decb15ee3ef83a751e0e9d0719d6
Levi126 <levi126@gmail.com>
2026-06-16 22:05:01 -0400

    Environment Change For BrowserStack

--- features/environment.py
+++ features/environment.py
@@ -3,11 +3,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
 from app.application import Application
 
-def browser_init(context):
+def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
@@ -19,8 +20,24 @@ def browser_init(context):
     # context.driver = webdriver.Chrome()
     # context.driver.maximize_window()
 
-    context.driver = webdriver.Firefox()
-    context.driver.maximize_window()
+    # context.driver = webdriver.Firefox()
+    # context.driver.maximize_window()
+
+    ### BROWSERSTACK INFO ###
+    bs_user = 'leviwilkerson_HEvT1s'
+    bs_key = 'siHyucCHPaT5YXMNrFAz'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+            "os": "Windows",
+            "osVersion": "11",
+            "browserVersion": "148.0",
+            'browserName': "Chrome",
+            'sessionName': scenario_name,
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.wait = WebDriverWait(context.driver, timeout=15)
 
@@ -30,7 +47,7 @@ def browser_init(context):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
 def before_step(context, step):
     print('\nStarted step: ', step)
