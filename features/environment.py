8350d0c8cc8066b69b636e1b3469b0e3b807de66
Levi126 <levi126@gmail.com>
2026-06-19 16:09:37 -0400

    Mobile Web Emulation Passed

--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
+# from selenium.webdriver.firefox.service import Service ### USED FOR FIREFOX BROWSER
+# from webdriver_manager.firefox import GeckoDriverManager ### USED FOR FIREFOX BROWSER
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -12,31 +13,38 @@ def browser_init(context, scenario_name):
     """
     :param context: Behave context
     """
+    ### HEADLESS MODE SYNTAX ###
     # chrome_options = Options()
     # chrome_options.add_argument("--headless=new")
     # context.driver = webdriver.Chrome(options=chrome_options)
     # context.driver.set_window_size(1920, 1080)
 
-    # context.driver = webdriver.Chrome()
-    # context.driver.maximize_window()
+    ### MOBILE EMULATION ###
+    mobile_emulation = {"deviceName": "iPhone X"}
+
+    chrome_options = Options()
+    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+
+    context.driver = webdriver.Chrome(options=chrome_options)
+
 
     # context.driver = webdriver.Firefox()
     # context.driver.maximize_window()
 
     ### BROWSERSTACK INFO ###
-    bs_user = 'leviwilkerson_HEvT1s'
-    bs_key = 'siHyucCHPaT5YXMNrFAz'
-    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-
-    options = Options()
-    bstack_options = {
-            "osVersion" : "16",
-            "deviceName" : "iPhone 14 Plus",
-            'browserName': "safari",
-            'sessionName': scenario_name,
-    }
-    options.set_capability('bstack:options', bstack_options)
-    context.driver = webdriver.Remote(command_executor=url, options=options)
+    # bs_user = 'leviwilkerson_HEvT1s'
+    # bs_key = 'siHyucCHPaT5YXMNrFAz'
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #         "osVersion" : "16",
+    #         "deviceName" : "iPhone 14 Plus",
+    #         'browserName': "safari",
+    #         'sessionName': scenario_name,
+    # }
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.wait = WebDriverWait(context.driver, timeout=15)
 
